2eb8f6ee2e25d6c767f60076fc7b96ea6a8d088e
Mayank Yadav <mayankyadav18101@gmail.com>
2025-08-04 21:58:08 +0530

    Program to take string as input and display it.cpp

---
+++ Taking string as input and display it.cpp
@@ -0,0 +1,21 @@
+/* Name - Mayank yadav
+   PRN - 24070123060
+   Class - ENTC A-3
+   Date - 31/07/2025 */
+#include <iostream>
+using namespace std;
+int main() {
+    char str[50];
+    cout << "Enter a string: ";
+    cin >> str;  
+    cout <<"You entered:"<< str << endl;
+    return 0;
+}
+
+
+
+/* OUTPUT
+Enter a string: DataStructures
+You entered:DataStructures
+
+ */
